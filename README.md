75a714184b91aa52569c2c82fd1097b1cac46f4b
huangjunxian <huangjunxian@datadriver.com.cn>
2022-08-23 11:29:16 +0800

    init project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# breadcrumb-demo
+# abc
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style lang="less">
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
