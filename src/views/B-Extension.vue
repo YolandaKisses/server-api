416a373ead7ea9406bb7ceee39ed9f59630826d7
huangjunxian <huangjunxian@datadriver.com.cn>
2022-08-23 11:24:22 +0800

    update

---
+++ src/views/B-Extension.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+  B-Extension
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/D-customer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>D-customer.vue</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
